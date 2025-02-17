e4263664b37d3814e62fe20832c0b15aff4da738
unknown <juliansalazarvelez94@gmail.com>
2025-02-16 19:07:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import './App.css'; // Estilos personalizados
+import Countdown from 'react-countdown';
+import Youtube from 'react-youtube';
+
+function App() {
+  const [email, setEmail] = useState('');
+
+  // Manejar el cambio de input
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // Manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`¡Gracias por registrarte, ${email}!`);
+    setEmail('');
+  };
+
+  const videoOptions = {
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      rel: 0,
+      showinfo: 0,
+      mute: 1,
+      loop: 1,
+      disableInitialPlay: false,
+      disableAutoPlay: false,
+      disableAds: true, 
+    },
+    height: '390',
+    width: '100%',
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>La historia que todos estaban esperando...</h1>
+        <p>Próximamente, descubre la mini serie local que te cautivará.</p>
+      </header>
+      <section className="content">
+        <Youtube videoId="cLd0dN25i5g" opts={videoOptions}/>
+        <h2>¡Únete a la espera!</h2>
+        <h2><Countdown date={'2025-06-01T00:00:00'}/></h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Introduce tu email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+          <button type="submit">Registrarme</button>
+        </form>
+      </section>
+
+      <footer>
+        <p>Síguenos en nuestras redes sociales:</p>
+        <div className="social-links">
+          <a href="https://www.facebook.com/AtlasCreativa" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://www.instagram.com/atlas_creativa/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
